7a0ab583d5e351976f5fa10ccd72eeee40ac4a5e
COMPUTER <msreddy0119@gmail.com>
2022-09-26 22:38:46 +0100

    RMS-123 This commit is for changing the user display message

--- src/test/java/Steps/MyStepdefs.java
+++ src/test/java/Steps/MyStepdefs.java
@@ -44,7 +44,7 @@ public class MyStepdefs {
     @And("^Click on post button$")
     public void click_on_post_button() throws Throwable {
         System.out.println("Click on post button");
-        System.out.println("This is the Modified Part of it");
+        System.out.println("This is the Modified part of it");
 
     }
 
