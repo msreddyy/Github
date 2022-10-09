0e556eeac8c306e4e95c13a86978c767924ca71f
msreddyy <114391930+msreddyy@users.noreply.github.com>
2022-10-09 14:26:31 +0100

    build 24

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with Jenkiiiiins");
         System.out.println("This demo for git rebase from Eclipse ");
+        System.out.println("Finally Build 24 successful ");
     }
 }
