09abae1c3395d81ae4b2962b579adb35b7f5c5ae
msreddyy <114391930+msreddyy@users.noreply.github.com>
2022-10-09 14:32:37 +0100

    creates faulty

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args)s {
         System.out.println("This is my first github commit integrating with Jenkiiiiins");
         System.out.println("This demo for git rebase from Eclipse ");
         System.out.println("Finally Build 24 successful ");
