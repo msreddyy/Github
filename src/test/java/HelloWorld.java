caa8e35aa15795409e3a557565422bb4a6637553
msreddyy <114391930+msreddyy@users.noreply.github.com>
2022-10-09 14:36:40 +0100

    fixed the broken build

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args)s {
+    public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with Jenkiiiiins");
         System.out.println("This demo for git rebase from Eclipse ");
         System.out.println("Finally Build 24 successful ");
