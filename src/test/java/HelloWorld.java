86a2aaedc0a3bf0ce13fdbe50a8f38e52ecf8c76
msreddyy <114391930+msreddyy@users.noreply.github.com>
2022-10-10 11:34:16 +0100

    demo for 2 failed

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with Jenkins");
-        System.out.println("This demo for git rebase from Eclipse ");
-        System.outtt.println("Finally Build 24 successful ");
+        Systemmm.out.println("This demo for git rebase from Eclipse ");
+        Systmem.outtt.println("Finally Build 24 successful ");
     }
 }
