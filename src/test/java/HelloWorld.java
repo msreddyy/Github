a27bfd38430d6b125efa13b0a097d9bc2d53b671
msreddyy <114391930+msreddyy@users.noreply.github.com>
2022-10-10 11:24:48 +0100

    Demo for broken pipeline

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with Jenkins");
         System.out.println("This demo for git rebase from Eclipse ");
-        System.out.println("Finally Build 24 successful ");
+        System.outtt.println("Finally Build 24 successful ");
     }
 }
