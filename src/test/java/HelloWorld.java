e9b36ab4c8ad0e8bc6d56c7b49b8a7bdc37f8caa
msreddyy <114391930+msreddyy@users.noreply.github.com>
2022-10-10 12:18:25 +0100

    Update HelloWorld.java

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
         System.out.println("This is my first github commit integrating with Jenkins");
         System.out.println("This demo for git rebase from Eclipse ");
         System.out.println("Finally Build 24 successful ");
-        Systmem.out.println("recover ");
+        System.out.println("recover ");
         
         
     }
