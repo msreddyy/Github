21385fcad687d60cf73c35ed6a38dad08571049e
COMPUTER <msreddy0119@gmail.com>
2023-01-05 13:16:25 +0000

    second time pushing the code

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is my first github commit integrating with Jenkins");
-        System.out.println("This demo for git rebase from Eclipse ");
-        System.out.println("Finally Build 24 successful ");
-        System.out.println("recover ");
+        System.out.println("This is my SECOND github commit integrating with Jenkins");
+        System.out.println("This demo222 for git rebase from Eclipse ");
+        System.out.println("Finally Build 2 successful ");
+        System.out.println("recover FINALLY ");
         
         
     }
